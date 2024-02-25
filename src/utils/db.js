69041dd622c7475faeba8696ba56322401fb40ff
Naufal Nabil Ramadhan <128666209+naufalnrsmitter20@users.noreply.github.com>
2024-02-25 20:05:14 +0700

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-let uri = "mongodb+srv://naufalnr:kcQQbd5tMTrrQFue@cluster0.nwavamo.mongodb.net/LansiCare?retryWrites=true&w=majority";
+let uri = process.env.MONGO_URL;
 
 const connect = async () => {
   if (mongoose.connections[0].readyState) return;
