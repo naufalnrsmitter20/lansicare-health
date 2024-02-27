5df16404ac8d26dac728fbb45be319759e80ce30
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-02-27 18:12:26 +0700

    hyhthy

--- src/app/administration/page.tsx
+++ src/app/administration/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { useSession, signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 import React from "react";
 
 export default function Admin() {
+  // const session = useSession({
+  //   required: true,
+  //   onUnauthenticated() {
+  //     redirect("admin/dashboard/login");
+  //   },
+  // });
   redirect("/administration/dashboard/insight");
   return <main className=" p-8"></main>;
 }
+Admin.requireAuth = true;
--- src/app/users/SessionProvider.tsx
+++ src/app/users/SessionProvider.tsx
@@ -1,10 +1,11 @@
-"use client";
 import { SessionProvider as Provider, SessionProvider } from "next-auth/react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function AuthProvider({ children }: any) {
-  return <SessionProvider>{children}</SessionProvider>;
+function NextAuthProvider({ children }: Props) {
+  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 }
+
+export default NextAuthProvider;
