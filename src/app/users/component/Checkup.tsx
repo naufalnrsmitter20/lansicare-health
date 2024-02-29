1d6039556a694ca13aa22f7b4a016d003db100ae
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-02-29 09:42:30 +0700

    commit

--- src/app/users/component/Checkup.tsx
+++ src/app/users/component/Checkup.tsx
@@ -14,8 +14,8 @@ export default function CheckupComp() {
         <div className="mx-auto flex h-full flex-col place-items-center justify-center bg-base-50 pb-[30px] pt-[70px] align-middle font-inter">
           {/* Fitur-fitur */}
           <div
-            id="main"
-            className="mx-[35] mt-[10px] h-auto w-[1366.48px] rounded-[10px] bg-primary-1000 pb-10 shadow-md"
+            id="checkup"
+            className="mx-[35] mt-[40px] h-auto w-[1366.48px] rounded-[10px] bg-primary-1000 pb-10 shadow-md"
           >
             <div className="mt-[33px] inline-flex h-6 w-96 flex-col items-start justify-start px-10">
               <div className="self-stretch text-2xl font-semibold leading-normal text-neutral-800">
@@ -23,6 +23,8 @@ export default function CheckupComp() {
               </div>
             </div>
           </div>
+
+          <div className=""></div>
         </div>
       </main>
     </>
